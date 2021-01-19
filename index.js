794129e658d441c3003fe285b7103e63800b0897
Himsnshu Sharma <iamonlyhimanshusharma@gmail.com>
2021-01-19 13:32:00 +0530

    added port and first home page for routing

---
+++ index.js
@@ -0,0 +1,44 @@
+/*Room on Rent is project which helps the people to finding the room (on rent),hotel,Pg,hostel acCording to thier choice */
+/* HIMANSHU KUMAR B.E 2021 */
+
+//Let`s Code//
+
+// importing the modules 
+
+require('dotenv').config(); //dot-env module 
+const express = require('express'); //Nodejs web application framework
+const morgan = require('morgan');//for  debugging 
+const path =require('path');//specify the path
+const chalk = require('chalk'); //beautify the console window
+
+
+const app = express();
+
+//useing middlewares
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname)));//for  the statics file (image ,videos etc..)
+
+
+//port
+
+const port = process.env.PORT || 80;
+
+
+//routing code...
+
+//for home page
+app.get('/',(req,res)=>{
+
+    res.send('hello this the first site..');
+
+
+})
+
+
+//listenig the port
+
+app.listen(port,()=>{
+    
+    console.log(chalk.bgBlackBright.cyanBright(process.env.SERVER_MESSAGE,port));
+})
