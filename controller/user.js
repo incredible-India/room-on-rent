e87a61dfdb22a0ed84ab08e565553f0f3e347952
Himsnshu Sharma <iamonlyhimanshusharma@gmail.com>
2021-01-20 22:50:15 +0530

    authentication code and cookies

--- controller/user.js
+++ controller/user.js
@@ -1,17 +1,24 @@
+/*this routing page is only for the users */
+//importing the modules which should be used in this code
 const express = require('express');
 const router = express.Router();//routing for urls
-const path =require('path');//for specify the path
-const multer  = require('multer');//for saving the image
+const path = require('path');//for specify the path
+const multer = require('multer');//for saving the image
 const bodyParser = require('body-parser');//parsing data from url
-const {check,validationResult} = require('express-validator');//it will check the form validation on server
-const newUserdbs  = require('./../model/newUser');//this is the shema of users
+const { check, validationResult } = require('express-validator');//it will check the form validation on server
+const newUserdbs = require('./../model/newUser');//this is the schema of users
 const fs = require('fs');//for file system module it help to save the image in our database
+const jwt = require('jsonwebtoken');//generate token and authenticate (varify) the user
+const cookieParser = require('cookie-parser');//saving the cookies
+
+
 
 // useing middlewaras 
 router.use(express.static(path.join(__dirname + './../')));
 
 router.use(bodyParser.json());//for parsing data in json formate
-router.use(bodyParser.urlencoded({extended:false}));
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(cookieParser());
 
 
 
@@ -19,10 +26,10 @@ router.use(bodyParser.urlencoded({extended:false}));
 
 var storage = multer.diskStorage({
     destination: 'UserImage'
-   
+
 
     , filename: (req, file, cb) => {
-        cb(null, Date.now() + "_user_"+file.originalname)
+        cb(null, Date.now() + "_user_" + file.originalname)
     }
 })
 
@@ -31,17 +38,17 @@ var uploads = multer({ storage: storage })
 //it will save images in our storage
 
 
-let uploadImage = multer({storage:storage});//this will save image
+let uploadImage = multer({ storage: storage });//this will save image
 
 
 
 
 // routing code for the users 
-router.get('/newusers', (req,res)=>{// for the newUsers
- 
-    res.setHeader('Content-Type',"text/html");
-    
-    res.status(200).sendFile(path.join(__dirname,"./../src/html/",'newUser.html'));  //registration form
+router.get('/newusers', (req, res) => {// for the newUsers
+
+    res.setHeader('Content-Type', "text/html");
+
+    res.status(200).sendFile(path.join(__dirname, "./../src/html/", 'newUser.html'));  //registration form
 
 
 
@@ -49,18 +56,18 @@ router.get('/newusers', (req,res)=>{// for the newUsers
 
 //for the login page
 
-router.get('/login',(req,res)=>{
+router.get('/login', (req, res) => {
 
-    res.setHeader('Content-Type',"text/html");
+    res.setHeader('Content-Type', "text/html");
 
-    res.status(200).sendFile(path.join(__dirname,"./../src/html/",'login.html'));  //registration form
+    res.status(200).sendFile(path.join(__dirname, "./../src/html/", 'login.html'));  //registration form
 })
 
 
 
 //new User registration form will varify here 
 
-router.post('/varify/data/users',uploads.single('uimg'),[
+router.post('/varify/data/users', uploads.single('uimg'), [
 
     check('fname').not().isEmpty().trim(),
     check('lname').not().isEmpty().trim(),
@@ -71,45 +78,75 @@ router.post('/varify/data/users',uploads.single('uimg'),[
     check('city').not().isEmpty().trim(),
     check('zip').not().isEmpty().trim(),
 
-], (req,res)=>{
+], (req, res) => {
 
     const errors = validationResult(req);
 
-   if( !errors.isEmpty()){  //if any error found for validating user data
+    if (!errors.isEmpty()) {  //if any error found for validating user data
 
-    return res.json({
-        status : false,
+        return res.json({
+            status: false,
 
-        message: "validation error",
+            message: "validation error in registration",
 
-        error :errors.array()
-    })
+            error: errors.array()
+        })
 
-}
-
-
-//after varifying the data we will save the data in our database
-return res.send(req.file)
-
-let SaveUserinfo = new newUserdbs({
-
-    fname :req.body.fname,
-    lname :req.body.lname,
-    password:req.body.password,
-    cnfpassword:req.body.cnfpassword,
-    email :req.body.email,
-    mobile :req.body.mobile,
-    city :req.body.city,
-    zip :req.body.zip,
-    img :{
-        data : fs.readFileSync(path.join(__dirname, '../userImage/', req.file.filename))
-        ,ContentType : "image/jpg"
     }
 
-})
+
+    //after varifying the data we will save the data in our database
+
+    // first we will check the email provided by user is exist or not
+
+
+    if (req.body.password === req.body.cnfpassword)//if password and confirm password are same then only we can save in database 
+    {
+        let SaveUserinfo = new newUserdbs({
+
+            fname: req.body.fname,
+            lname: req.body.lname,
+            password: req.body.password,
+            cnfpassword: req.body.cnfpassword,
+            email: req.body.email,
+            mobile: req.body.mobile,
+            city: req.body.city,
+            zip: req.body.zip,
+            gender: req.body.gender,
+            img: {
+                data: fs.readFileSync(path.join(__dirname, '../userImage/', req.file.filename))
+                , ContentType: "image/jpg"
+            }
+
+        })
+
+         const token = SaveUserinfo.generateTheToken();  //it will generate the token when user will registration itself
+
+         res.cookie('jwt',token); // generated token we will save in cookie and varify this latar when user will login
+
+
+        SaveUserinfo.save((err, data) => {
+
+            if (err) {
+                return res.json({
+
+                    message: "saving in database error numer 1 in registration",
+
+                    error: err
+
+                })
+            }
+
+
+            res.json(data)//yeha pe pug wala lagega 
+        })
+    } else {
+        
+        return res.send("password not matched with confirm password.")
+    }
 
 
 })
 
 
-module.exports =router; //will be import in index.js (main file)
+module.exports = router; //will be import in index.js (main file)
--- model/newUser.js
+++ model/newUser.js
@@ -1,5 +1,9 @@
 //Schema for the new user..
+require('dotenv').config();//reading the file to the database
 const mongoose = require('mongoose');//for the mongodb database
+const bcryptjs =require('bcryptjs');//bcrypte password before saving the data
+const jwt = require('jsonwebtoken');//jwt genrate and varify the token
+
 
 const schema = mongoose.Schema;//making new shemas for the new user
 
@@ -50,6 +54,11 @@ let newUser = new schema({
         type:Number,
         required:true
     }
+    ,
+    tokenSchema : [{tokendbs:{
+        type:String,
+        required:true
+    }}]
     
 
 
@@ -57,6 +66,33 @@ let newUser = new schema({
 })
 
 
+newUser.methods.generateTheToken = function()
+{
+   const tokenGenrate = jwt.sign({_id : this._id}, process.env.SECRET_KEY);//this will generate a token
+
+   //now save in database this token 
+
+   this.tokenSchema = this.tokenSchema.concat( {tokendbs : tokenGenrate} );
+
+   return tokenGenrate;
+
+//    this.save();
+
+
+}
+
+
+ newUser.pre('save', function(next){
+    console.log(this);
+
+ if(this.isModified('password'))
+ {
+     this.password =  bcryptjs.hashSync(this.password,10);
+    //  this.cnfpassword =bcryptjs.hashSync(this.cnfpassword);
+ }
+ next();
+
+})
 
 
 mongoose.model('users', newUser);
